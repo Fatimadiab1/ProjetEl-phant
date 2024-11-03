bcb0447975bec971b0d56010de1ee8c80a675fc8
neymounette <77481783+neymounette@users.noreply.github.com>
2024-11-03 23:10:09 +0100

    Commit gradel

--- Projet-elephants/details-max.php
+++ Projet-elephants/details-max.php
@@ -1,6 +1,148 @@
 <?php include 'header.php' ?>
 
 
+<!-- background en dessous de la nav -->
+<div class="background-player">
+</div>
 
 
-<?php include 'footer.php' ?>
+<!-- Titre de la position des joueurs  -->
+<div class="position-player">
+<h1 class="titre-position">MILIEU</h1>
+</div>
+
+
+<!-- Premiere section  -->
+<div class="firstsection-player">
+
+<div class="picture-player">
+<img class="pic" src="img/Max_Gradel_9292.jpeg" alt="">
+</div>
+
+<div class="info-player">
+<h2 class="name">Max-Alain <br> Gradel</h2>
+<br>
+<p class="description">Max-Alain Gradel, né le 30 novembre 1987 à Abidjan est un <br> footballeur international ivoirien qui évolue au poste <br> d'aillier gauche à  Sakaryaspor. </p>
+<br>
+<br>
+<p class="description">
+Max-Alain Gradel a joué un rôle décisif lors de la CAN 2024, <br>
+agissant en tant que leader et moteur de l'équipe ivoirienne. <br>
+Son expérience et son talent se sont manifestés à travers des buts<br>
+et des passes décisives clés, tout en motivant ses coéquipiers.<br>
+Sa capacité à créer des occasions et à maintenir la <br>
+cohésion de l'équipe a été essentielle pour le succès de la Côte d'Ivoire <br> dans cette compétition.
+</p>
+
+</div>
+
+</div>
+
+<!-- Deuxieme section -->
+<div class="secondsection-player">
+
+<div class="palmares">
+<h3 class="titre-palmares">MATCHES AVEC <br> LA COTE D'IVOIRE</h3>
+<p class="nbr">49</p>
+<p class="saison">SAISON 2024/2025 <span>15</span></p>
+</div>
+
+<div class="palmares">
+<h3 class="titre-palmares">BUTS AVEC  LA <br>COTE D'IVOIRE</h3>
+<p class="nbr">09</p>
+<p class="saison">SAISON 2024/2025<span>02</span></p>
+</div>
+
+<div class="palmares">
+<h3 class="titre-palmares">PASSES DECISIVES AVEC <br> LA COTE D'IVOIRE</h3>
+<p class="nbr">05</p>
+<p class="saison">SAISON 2024/2025 <span>02</span></p>
+</div>
+
+</div>
+
+<!-- Troisieme section -->
+
+<h2 class="newstitle">DERNIERES INFOS</h2>
+
+ <div class="thirdsection-player">
+
+<div class="image-container">
+        <img src="img/GRADEL.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Passé de remplaçant à titulaire et capitaine en plein milieu de la CAN, Max-Alain Gradel prouve qu’à 36 ans, il a encore les qualités pour se montrer essentiel à son équipe.</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/Gradelnews.png" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Lors de la CAN il a eu un acte décisif a été de marquer un penalty crucial lors de la séance de tirs au but, contribuant ainsi à la victoire de son équipe</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/Gradelelephantcan.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Gradel a également été un joueur important tout au long du tournoi, apportant vitesse et créativité à l'attaque ivoirienne.</h2>
+        </div>
+    </div>
+
+    </div>
+
+<!-- Carouselle -->
+ <div class="fourthsection-player">
+ <swiper-container class="mySwiper" slides-per-view="3" centered-slides="true" space-between="30" pagination="true"
+      pagination-type="fraction" navigation="true" style="margin-top: 40px;margin-bottom:40px;">
+      <swiper-slide>
+          <a href="img/max-gradel1.png" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/max-gradel1.png" alt="">
+          </a>
+         
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/maxalaingradel.png" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/maxalaingradel.png" alt="">
+          </a>
+          
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/Max-Alain-Gradel.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/Max-Alain-Gradel.jpg" alt="">
+          </a>
+
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/MaxAlainGRad.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/MaxAlainGRad.jpg" alt="">
+          </a>
+   
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/GradelElephant.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/GradelElephant.jpg" alt="">
+          </a>
+
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/Gradelcoupe.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/Gradelcoupe.jpg" alt="">
+          </a>
+          </swiper-slide>
+
+  </swiper-container>
+  </div>
+<!-- footer -->
+  <?php include 'footer.php' ?>
+
+
+  <!-- Javascript -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+
+  <script src="js/script.js"></script>
+
