68eff57a321a8b5649fc166014510fd29c6d6037
Chawki <khalifechawki2004@outlookfr>
2024-11-04 16:56:27 +0100

    Joueurs ivoiriens

--- Projet-elephants/details-lazare.php
+++ Projet-elephants/details-lazare.php
@@ -1,6 +1,148 @@
 <?php include 'header.php' ?>
 
 
+<!-- background en dessous de la nav -->
+<div class="background-player">
+</div>
 
 
+<!-- Titre de la position des joueurs  -->
+<div class="position-player">
+    <h1 class="titre-position">Milieu</h1>
+</div>
+
+
+<!-- Premiere section  -->
+<div class="firstsection-player">
+
+    <div class="picture-player">
+        <img class="pic" src="img/lazare_amani.png" alt="">
+    </div>
+
+    <div class="info-player">
+        <h2 class="name">Lazare <br> Amani</h2>
+        <br>
+        <p class="description">Jean Thierry Lazare Amani (né le 7 mars 1998) <br>est un footballeur professionnel ivoirien qui joue pour le club belge de Pro League Union Saint-Gilloise. </p>
+        <br>
+        <br>
+        <p class="description">
+            Milieu de terrain talentueux de l'Union Saint-Gilloise en Belgique<br>
+            Lazare a récemment fait ses débuts pour la Côte d'Ivoire<br>
+            impressionnant dès sa première apparition en Coupe d’Afrique des Nations (CAN) 2023. <br>
+            il montre une progression impressionnante et attire l’attention sur la scène <br>
+            Actuellement, Lazare joue un rôle clé dans l’équipe nationale et a été <br>
+            sélectionné pour renforcer les Éléphants lors de la CAN en 2024.
+        </p>
+
+    </div>
+
+</div>
+
+<!-- Deuxieme section -->
+<div class="secondsection-player">
+
+    <div class="palmares">
+        <h3 class="titre-palmares">MATCHES AVEC <br> LA COTE D'IVOIRE</h3>
+        <p class="nbr">15</p>
+        <p class="saison">SAISON 2024/2025 <span>11</span></p>
+    </div>
+
+    <div class="palmares">
+        <h3 class="titre-palmares">BUTS AVEC LA <br>COTE D'IVOIRE</h3>
+        <p class="nbr">00</p>
+        <p class="saison">SAISON 2024/2025<span>00</span></p>
+    </div>
+
+    <div class="palmares">
+        <h3 class="titre-palmares">PASSES DECISIVES AVEC <br> LA COTE D'IVOIRE</h3>
+        <p class="nbr">00</p>
+        <p class="saison">SAISON 2024/2025 <span>00</span></p>
+    </div>
+
+</div>
+
+<!-- Troisieme section -->
+
+<h2 class="newstitle">DERNIERES INFOS</h2>
+
+<div class="thirdsection-player">
+
+    <div class="image-container">
+        <img src="img/lazare_amani1.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text"> Amani fait l'objet de rumeurs de transfert en ce moment. Après une saison prometteuse, plusieurs sources indiquent qu'il pourrait quitter le club.</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/lazare_amani3.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Ce changement pourrait intervenir suite aux mouvements internes de l'Union Saint-Gilloise, tels que le transfert de Cameron Puertas.</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/lazare_amani5.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Son retour en équipe nationale a eu un impact positif, renforçant l'équipe et inspirant ses coéquipiers pendant la compétition.</h2>
+        </div>
+    </div>
+
+</div>
+
+<!-- Carouselle -->
+<div class="fourthsection-player">
+    <swiper-container class="mySwiper" slides-per-view="3" centered-slides="true" space-between="30" pagination="true"
+        pagination-type="fraction" navigation="true" style="margin-top: 40px;margin-bottom:40px;">
+        <swiper-slide>
+            <a href="img/lazare_amani.png" data-fancybox data-caption="Can 2023 // Ivoirien">
+                <img src="img/lazare_amani.png" alt="">
+            </a>
+
+        </swiper-slide>
+
+        <swiper-slide>
+            <a href="img/lazare_amani1.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+                <img src="img/lazare_amani1.jpg" alt="">
+            </a>
+
+        </swiper-slide>
+
+        <swiper-slide>
+            <a href="img/lazare_amani3.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+                <img src="img/lazare_amani3.jpg" alt="">
+            </a>
+
+        </swiper-slide>
+
+        <swiper-slide>
+            <a href="img/lazare_amani5.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+                <img src="img/lazare_amani5.jpg" alt="">
+            </a>
+
+        </swiper-slide>
+
+        <swiper-slide>
+            <a href="img/ouss_dio1.png" data-fancybox data-caption="Can 2023 // Ivoirien">
+                <img src="img/ouss_dio1.png" alt="">
+            </a>
+
+        </swiper-slide>
+
+        <swiper-slide>
+            <a href="img/lazare_amani6.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+                <img src="img/lazare_amani6.jpg" alt="">
+            </a>
+
+        </swiper-slide>
+
+    </swiper-container>
+</div>
+<!-- footer -->
 <?php include 'footer.php' ?>
+
+
+<!-- Javascript -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
--- Projet-elephants/details-ousmane.php
+++ Projet-elephants/details-ousmane.php
@@ -1,6 +1,149 @@
 <?php include 'header.php' ?>
 
 
+<!-- background en dessous de la nav -->
+<div class="background-player">
+</div>
 
 
-<?php include 'footer.php' ?>
+<!-- Titre de la position des joueurs  -->
+<div class="position-player">
+<h1 class="titre-position">Defenseur</h1>
+</div>
+
+
+<!-- Premiere section  -->
+<div class="firstsection-player">
+
+<div class="picture-player">
+<img class="pic" src="img/ouss_dio.jpg" alt="">
+</div>
+
+<div class="info-player">
+<h2 class="name">Ousmane <br> Diomande</h2>
+<br>
+<p class="description">Ousmane Diomande, né le 4 décembre 2003 à Abidjan est un <br> footballeur international ivoirien qui évolue au poste <br> de defenseur à Sporting CP.</p>
+<br>
+<br>
+<p class="description">
+     Ousmane Diomandé s’impose cette saison comme un pilier de la défense <br>
+     grâce à sa puissance et à sa régularité.Avec 13 matchs joués, 1 but et<br>
+     1 passe décisive en championnat portugais pour la saison 2024/2025, <br>
+     il montre une progression impressionnante et attire l’attention sur la scène <br>
+     internationale. Sa sélection pour la CAN 2024 sous la direction de Jean-Louis Gasset  <br>
+     témoigne de son impact et de son potentiel pour solidifier la défense ivoirienne.
+</p>
+
+</div>
+
+</div>
+
+<!-- Deuxieme section -->
+<div class="secondsection-player">
+
+<div class="palmares">
+<h3 class="titre-palmares">MATCHES AVEC <br> LA COTE D'IVOIRE</h3>
+<p class="nbr">07</p>
+<p class="saison">SAISON 2024/2025 <span>02</span></p>
+</div>
+
+<div class="palmares">
+<h3 class="titre-palmares">BUTS AVEC  LA <br>COTE D'IVOIRE</h3>
+<p class="nbr">00</p>
+<p class="saison">SAISON 2024/2025<span>00</span></p>
+</div>
+
+<div class="palmares">
+<h3 class="titre-palmares">PASSES DECISIVES AVEC <br> LA COTE D'IVOIRE</h3>
+<p class="nbr">00</p>
+<p class="saison">SAISON 2024/2025 <span>00</span></p>
+</div>
+
+</div>
+
+<!-- Troisieme section -->
+
+<h2 class="newstitle">DERNIERES INFOS</h2>
+
+ <div class="thirdsection-player">
+
+<div class="image-container">
+        <img src="img/ouss_dio1.png" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Arsenal a tenté de le recruter cet été avec une offre de 35 millions d'euros, mais le Sporting a refusé.</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/ouss_dio2.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Arrivé au Sporting en provenance de Midtjylland pour 7,5 millions d'euros en janvier 2023, il est vite devenu un élément central de l'équipe.</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/ouss_dio3.png" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Son retour en équipe nationale a eu un impact positif, renforçant l'équipe et inspirant ses coéquipiers pendant la compétition.</h2>
+        </div>
+    </div>
+
+    </div>
+
+<!-- Carouselle -->
+ <div class="fourthsection-player">
+  <swiper-container class="mySwiper" slides-per-view="3" centered-slides="true" space-between="30" pagination="true"
+      pagination-type="fraction" navigation="true" style="margin-top: 40px;margin-bottom:40px;">
+      <swiper-slide>
+          <a href="img/ouss_dio4.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/ouss_dio4.jpg" alt="">
+          </a>
+         
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/ouss_dio6.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/ouss_dio6.jpg" alt="">
+          </a>
+          
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/ouss_dio3.png" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/ouss_dio3.png" alt="">
+          </a>
+
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/ouss_dio1.png" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/ouss_dio1.png" alt="">
+          </a>
+   
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/ouss_dio1.png" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/ouss_dio1.png" alt="">
+          </a>
+
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/ouss_dio.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/ouss_dio.jpg" alt="">
+          </a>
+
+      </swiper-slide>
+
+  </swiper-container>
+  </div>
+<!-- footer -->
+  <?php include 'footer.php' ?>
+
+
+  <!-- Javascript -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+
+  <script src="js/script.js"></script>
+
