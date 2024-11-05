a85011fe5b768c7fd4a0089d8affa638988789f8
Marie-Emmanuelle2023 <azohmvnou@gmail.com>
2024-11-05 09:34:25 +0000

    je fais un commit

--- Projet-elephants/staff-technique.php
+++ Projet-elephants/staff-technique.php
@@ -10,9 +10,9 @@
 
 <div class="staff">
     <div class="emerse">
-        <div>
-            <img src="" alt="">
-            <div>
+        <div class="card">
+            <img src="img/emerse.jpg" alt="emerse fae">
+            <div class="text">
                 <h4>EMERSE FAÉ</h4>
                 <p>(Entraineur Principal)</p>
             </div>
@@ -20,23 +20,25 @@
     </div>
 
     <div class="entraineurs">
-        <div>
-            <img src="" alt="">
-            <div>
+        <div class="card">
+            <img src="img/guy.jpg" alt="guy demel">
+            <div class="text">
                 <h4>GUY DEMEL</h4>
                 <p>(Entraineur Adjoint)</p>
             </div>
         </div>
-        <div>
-            <img src="" alt="">
-            <div>
+
+        <div class="card">
+            <img src="img/alaing.jpg" alt="alain gouamene">
+            <div class="text">
                 <h4>ALAIN GOUAMENE</h4>
                 <p>(Entraineur Adjoint)</p>
             </div>
         </div>
-        <div>
-            <img src="" alt="">
-            <div>
+
+        <div class="card">
+            <img src="img/gerard.jpg" alt="gerard gnanhouan">
+            <div class="text">
                 <h4>GÉRARD GNANHOUAN</h4>
                 <p>(Entraineur des Gardiens)</p>
             </div>
@@ -44,17 +46,17 @@
     </div>
 
     <div class="preparateurs">
-        <div>
-            <img src="" alt="">
-            <div>
+        <div class="card">
+            <img src="img/samir.png" alt="samir anba">
+            <div class="text">
                 <h4>SAMIR ANBA</h4>
                 <p>(Préparateur Physique)</p>
             </div>
         </div>
 
-        <div>
-            <img src="" alt="">
-            <div>
+        <div class="card">
+            <img src="img/jeremy.png" alt="jeremy antonio">
+            <div class="text">
                 <h4>JEREMY ANTONIO</h4>
                 <p>(Préparateur Physique)</p>
             </div>
