39d335b4a08dfb9e2df41eb98318a6eac9c6bf1d
kon805 <kdaou048@gmail.com>
2024-11-05 10:14:42 +0000

    commit de bolly

--- Projet-elephants/details-willy.php
+++ Projet-elephants/details-willy.php
@@ -1,6 +1,148 @@
-<?php include 'header.php' ?>
+
+<?php
+$title = "Les éléphants | willy bolly";
+?><?php include 'header.php' ?>
 
 
+<!-- background en dessous de la nav -->
+<div class="background-player">
+</div>
 
+
+<!-- Titre de la position des joueurs  -->
+<div class="position-player">
+<h1 class="titre-position">ATTAQUANT</h1>
+</div>
+
+
+<!-- Premiere section  -->
+<div class="firstsection-player">
+
+<div class="picture-player">
+<img class="pic" src="img/bog4.jpg" alt="">
+</div>
+
+<div class="info-player">
+<h2 class="name">Jérémie <br> Boga</h2>
+<br>
+<p class="description"> Jérémie Boga, né le 3 janvier 1997 à Marseille,<br> est un footballeur international ivoirien, qui possède <br> également la nationalité française. Il évolue au poste de <br>milieu de terrain à l'OGC Nice.</p>
+                      
+<br>
+<br>
+<p class="description">
+     Boga participe à sa première compétition internationale lors de la   <br>
+     Coupe d'Afrique des nations 2021  au Cameroun, les ivoiriens seront <br>
+     éliminés en huitièmes de finale par l'Égypte aux tirs au but. <br>
+     Le 28 décembre 2023, il est retenu dans la liste des vingt-sept joueurs ivoiriens<br>
+     sélectionnés par Jean-Louis Gasset pour disputer la Coupe d'Afrique <br>des nations 2023 .
+</p>
+
+</div>
+
+</div>
+
+<!-- Deuxieme section -->
+<div class="secondsection-player">
+
+<div class="palmares">
+<h3 class="titre-palmares">MATCHES AVEC <br> LA COTE D'IVOIRE</h3>
+<p class="nbr">21</p>
+<p class="saison">SAISON 2024/2025 <span>00</span></p>
+</div>
+
+<div class="palmares">
+<h3 class="titre-palmares">BUTS AVEC  LA <br>COTE D'IVOIRE</h3>
+<p class="nbr">04</p>
+<p class="saison">SAISON 2023/2024<span>01</span></p>
+</div>
+
+<div class="palmares">
+<h3 class="titre-palmares">PASSES DECISIVES AVEC <br> LA COTE D'IVOIRE</h3>
+<p class="nbr">05</p>
+<p class="saison">SAISON 2022/2023 <span>02</span></p>
+</div>
+
+</div>
+
+<!-- Troisieme section -->
+
+<h2 class="newstitle">DERNIERES INFOS</h2>
+
+ <div class="thirdsection-player">
+
+<div class="image-container">
+        <img src="img/jeremie.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">L'OGC Nice a officialisé l'arrivée de Jérémie Boga en provenance de l'Atalanta. Le montant final du transfert devrait se situer autour des 20M€..</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/boga.jpeg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">C’est un Éléphant qui dribble énormément, et au stade de la Beaujoire, ce week-end du 2-3 décembre, c’était l’un des Niçois à surveiller de près lors du match Nantes-Nice (0-1) : Jérémie Boga.</h2>
+        </div>
+    </div>
+
+    <div class="image-container">
+        <img src="img/bog1.jpg" alt="Image seko fofana" class="imgplayer">
+        <div class="overlay">
+            <h2 class="overlay-text">Son retour en équipe nationale a eu un impact positif, renforçant l'équipe et inspirant ses coéquipiers pendant la compétition.</h2>
+        </div>
+    </div>
+
+    </div>
+
+<!-- Carouselle -->
+ <div class="fourthsection-player">
+  <swiper-container class="mySwiper" slides-per-view="3" centered-slides="true" space-between="30" pagination="true"
+      pagination-type="fraction" navigation="true" style="margin-top: 40px;margin-bottom:40px;">
+      <swiper-slide>
+          <a href="img/bog6.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/bog6.jpg" alt="">
+          </a>
+         
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/bog1.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/bog1.jpg" alt="">
+          </a>
+          
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/bog5.webp" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/bog5.webp" alt="">
+          </a>
+
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/Bo_1.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/Bo_1.jpg" alt="">
+          </a>
+   
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/bog3.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/bog3.jpg" alt="">
+          </a>
+
+      </swiper-slide>
+
+      <swiper-slide>
+          <a href="img/bog4.jpg" data-fancybox data-caption="Can 2023 // Ivoirien">
+              <img src="img/bog4.jpg" alt="">
+          </a>
+
+      </swiper-slide>
+
+  </swiper-container>
+  </div>
 
 <?php include 'footer.php' ?>
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
