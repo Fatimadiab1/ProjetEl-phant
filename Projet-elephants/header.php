aa921f6edfc59b4b5e6ff471624231169bdde058
Fatima <diabfatima934@gmail.com>
2024-11-06 22:15:01 +0000

    commitefatima

--- Projet-elephants/header.php
+++ Projet-elephants/header.php
@@ -13,14 +13,14 @@
     rel="stylesheet"
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/joueur.css">
-  <link rel="stylesheet" href="css/staff.css">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/joueur.css?v=1.0">
+  <link rel="stylesheet" href="css/staff.css">
 </head>
 
 <body>
