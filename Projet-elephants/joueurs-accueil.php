802ca96934d9fbd229a267d02d9caca60301c43f
Mardochée N'goran <mordekhai.ngoran@gmail.com>
2024-11-08 15:34:35 +0000

    Accordeon des postes des joueurs dans la page joueurs

--- Projet-elephants/joueurs-accueil.php
+++ Projet-elephants/joueurs-accueil.php
@@ -6,11 +6,11 @@ $title = "Les éléphants | joueurs";
 
 
 <style>
-  * {
+  /* * {
     margin: 0;
     padding: 0;
 
-  }
+  } */
 
   .terrain {
     background-image: url('img/squad2.jpg');
@@ -34,15 +34,15 @@ $title = "Les éléphants | joueurs";
 
   }
 
-  button {
+  /* button {
     padding-left: 18px;
     padding-right: 18px;
     padding-top: 8px;
     padding-bottom: 8px;
     margin: 20px;
-  }
+  } */
 
-  h1 {
+  /* h1 {
     color: orangered;
     text-align: center;
     font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
@@ -60,11 +60,11 @@ $title = "Les éléphants | joueurs";
     text-decoration: none;
     color: black;
 
-  }
+  } */
 
-  a:hover {
+  /* a:hover {
     color: white;
-  }
+  } */
 
   .btn {
     transition: transform 0.2s;
@@ -169,8 +169,116 @@ $title = "Les éléphants | joueurs";
 </div>
 
 
+<div class="container">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="accordion accordion-flush" id="accordionFlushExample">
+				<div class="accordion-item">
+					<h4 class="accordion-header">
+						<button class="accordion-button collapsed btn btn-danger" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+							ATTAQUANTS
+						</button>
+					</h4>
+					<div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+						<div class="accordion-body">
+							<div class="card card-body mb-3">
+								<ul class="list-unstyled text-center" style="color: orangered;">
+									<li><a href="details-sebastien.php">S.HALLER</a></li>
+									<li><a href="details-jeanphilippe.php">J.KRASSO</a></li>
+									<li><a href="details-simon.php">S.ADINGRA</a></li>
+									<li><a href="details-jeremie.php">J.BOGA</a></li>
+									<li><a href="details-jonathan.php">J.BAMBA</a></li>
+									<li><a href="details-nicolas.php">N.PEPE</a></li>
+									<li><a href="details-karim.php">K.KONATE</a></li>
+									<li><a href="details-max.php">M.GRADEL</a></li>
+									<li><a href="details-oumar.php">O.DIAKITE</a></li>
+									<li><a href="details-christian.php">K.CHRISTAN</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="accordion accordion-flush" id="accordionFlushExampleTwo">
+				<div class="accordion-item">
+					<h4 class="accordion-header">
+						<button class="accordion-button collapsed btn btn-danger" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+							MILIEUX
+						</button>
+					</h4>
+					<div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExampleTwo">
+						<div class="accordion-body">
+							<div class="card card-body mb-3">
+							<ul class="list-unstyled text-center" style="color: orangered;">
+								<li><a href="details-seko.php">S.FOFANA</a></li>
+								<li><a href="details-jeanmichael.php">J.SERI</a></li>
+								<li><a href="details-idrissa.php">I.DOUMBIA</a></li>
+								<li><a href="details-ibrahim.php">I.SANGARE</a></li>
+								<li><a href="details-franck.php">F.KESSIE</a></li>
+								<li><a href="details-lazare.php">L.AMANI</a></li>
+							</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="accordion accordion-flush" id="accordionFlushExampleThree">
+				<div class="accordion-item">
+					<h4 class="accordion-header">
+						<button class="accordion-button collapsed btn btn-danger" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+							DÉFENSEURS
+						</button>
+					</h4>
+					<div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExampleThree">
+						<div class="accordion-body">
+							<div class="card card-body mb-3">
+							<ul class="list-unstyled text-center " style="color: orangered;">
+								<li><a href="details-evan.php">E.N'DICKA</a></li>
+								<li><a href="details-willy.php">W.BOLLY</a></li>
+								<li><a href="details-odilon.php">O.KOSSONOU</a></li>
+								<li><a href="details-ousmane.php">O.DIOMANDE</a></li>
+								<li><a href="details-wilfried.php">W.SINGO</a></li>
+								<li><a href="details-serge.php">S.AURIER</a></li>
+								<li><a href="details-ghislain.php">G.KONAN</a></li>
+								<li><a href="details-idrissa.php">I.DIALLO</a></li>
+							</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="accordion accordion-flush" id="accordionFlushExampleFour">
+				<div class="accordion-item">
+					<h4 class="accordion-header">
+						<button class="accordion-button collapsed btn btn-danger" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+							GARDIENS
+						</button>
+					</h4>
+					<div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExampleFour">
+						<div class="accordion-body">
+							<div class="card card-body mb-3">
+							<ul class="list-unstyled text-center ">
+								<li><a href="details-yahia.php">Yahia FOFANA</a></li>
+								<li><a href="details-badra.php">Badra ALI</a></li>
+								<li><a href="details-charles.php">Charles FOLLY</a></li>
+							</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-<div class="containerr mt-5">
+
+<!-- <div class="containerr mt-5">
   <div class="text-center mb-4 d-flex flex-wrap justify-content-around" style="width: 100%;">
     <button class="btn btn-primary m-2" data-toggle="collapse" data-target="#section1" style="font-size: 18px;">ATTAQUANTS</button>
     <button class="btn btn-secondary m-2" data-toggle="collapse" data-target="#section2" style="font-size: 18px;">MILIEUX</button>
@@ -241,7 +349,7 @@ $title = "Les éléphants | joueurs";
     </div>
   </div>
 
-</div>
+</div> -->
 
 
 
